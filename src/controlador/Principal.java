de532ac6ede1446ec704ce53ac1bc8a5969154c2
Alvaroskill <allinfer22@gmail.com>
2022-04-27 13:40:56 +0200

    Segundo commit de prueba con hola mundo y variable nombre

--- src/controlador/Principal.java
+++ src/controlador/Principal.java
@@ -9,11 +9,15 @@ package controlador;
  */
 public class Principal {
 
+	String nombre = "Pilar";
+	
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.print("Hola mundo");
 
 	}
 
