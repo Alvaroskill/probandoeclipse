8790ee5757458c2d2cc650c7c72972ae91badc4d
Alvaroskill <allinfer22@gmail.com>
2022-04-27 13:36:04 +0200

    Prueba

---
+++ src/controlador/Principal.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package controlador;
+
+/**
+ * @author 1DAM
+ *
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
